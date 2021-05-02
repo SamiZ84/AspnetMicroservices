d09fcd408156b1724698cacd35f8201834a13495
Sami ZOUAOUI <EX01057@actionlogement.fr>
2021-05-02 04:03:57 +0200

    initialize db postgresql in program.cs

--- Source/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
+++ Source/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -3,8 +3,6 @@ using Discount.Api.Entities;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Discount.Api.Repositories
